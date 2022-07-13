050da528f790b09de2d83595a5183bf8acbff19f
Jake <jacob.willard.gr@dartmouth.edu>
2022-07-13 00:17:09 -0400

    streamplot animation

--- my_julia_defs.jl
+++ my_julia_defs.jl
@@ -49,106 +49,46 @@ struct SimData
 
 end
 
-@recipe function f(::Type{Val{:magplot}}, xvals, yvals, Vx, Vy)
+function animate_magnetic_field(dat::SimData, path; framerate=30)
 
-    Vmag = sqrt.(Vx.^2 + Vy.^2)
-    x := xvals
-    y := yvals
-    z := Vmag
-    seriestype := heatmap
-    ()
-end
-
-
-
-
-
-
-
-
-
-
-
-
-@userplot BPlot
-@recipe function f(bp::BPlot)
-
-    dat = bp.args[1]
-    t = bp.args[2]
-
-    Bx = dat.Bx[:,:,t]
-    By = dat.By[:,:,t]
-    B = sqrt.(Bx.^2 + By.^2)
-    bx = Bx ./ B
-    by = By ./ B
     x = dat.xvals
     y = dat.yvals
+    dx = x[2] - x[1]
+    dy = y[2] - y[1]
 
-    @series begin
-        seriestype := heatmap
-        x, y, transpose(B)
-    end
+    fig = Figure()
+    ax1 = Axis(fig[1, 1])
+    ax2 = Axis(fig[2, 1])
 
-    Nx = length(x)
-    Ny = length(y)
-    X = hcat([x for i=1:Ny]...)
-    Y = vcat([transpose(y) for i=1:Nx]...)
-    xvec = reshape(X, (Nx*Ny,1))
-    yvec = reshape(Y, (Nx*Ny, 1))
-    bvec = hcat(reshape(bx, (Nx*Ny,1)), reshape(by, (Nx*Ny,1)))
+    record(fig, path, [1:dat.Nt...], framerate=framerate) do t
 
-    @series begin
-        seriestype := quiver
-        xvec, yvec, bvec
-    end
-end
+        Bx = interpolate(dat.Bx[:,:,t], BSpline(Linear()))
+        By = interpolate(dat.By[:,:,t], BSpline(Linear()))
+        Ux = interpolate(dat.Ux[:,:,t], BSpline(Linear()))
+        Uy = interpolate(dat.Uy[:,:,t], BSpline(Linear()))
+        rho = dat.rho[:,:,t]
+        P = dat.P[:,:,t]
 
-
-function plot_B(x, y, Bx, By; scale=0.05, cut=1)
-
-    Nx = length(x)
-    Ny = length(y)
-
-    B = sqrt.(Bx.^2 + By.^2)
-    bx = Bx ./ B
-    by = By ./ B
-
-    h = heatmap(x, y, transpose(B))
-
-    is = 1:cut:Nx
-    js = 1:cut:Ny
-    Nk = length(is)*length(js)
-    println(Nk)
-    xs = zeros((2,Nk))
-    ys = zeros((2,Nk))
-
-    p = Progress(Nk, 0.5)
-    k = 1
-    for i=is
-        for j=js
-            xs[1, k] = x[i]
-            xs[2, k] = x[i] + scale*bx[i,j]
-            ys[1, k] = y[j]
-            ys[2, k] = y[j] + scale*by[i,j]
-            k += 1
-            next!(p)
+        b(p::Point2) = begin
+            i = 1 + (p[1] - x[1]) / dx
+            j = 1 + (p[2] - y[1]) / dy
+            bx = Bx(i,j)
+            by = By(i,j)
+            Point2(bx, by)
         end
+
+        v(p::Point2) = begin
+            i = 1 + (p[1] - x[1]) / dx
+            j = 1 + (p[2] - y[1]) / dy
+            vx = Ux(i,j)
+            vy = Uy(i,j)
+            Point2(vx, vy)
+        end
+        delete!(ax1)
+        delete!(ax2)
+        ax1 = Axis(fig[1, 1])
+        ax2 = Axis(fig[2, 1])
+        streamplot!(ax1, b, x[1]..x[end], y[1]..y[end])
+        streamplot!(ax2, v, x[1]..x[end], y[1]..y[end])
     end
-
-    #plot!(h, xs, ys, arrow=true, legend=false)
-    return h, xs, js
-end
-
-
-
-function animate_B(sim::SimData)
-
-    Nt = size(sim.Bx)[3]
-    anim = @animate for t=1:Nt
-        B = sqrt.(sim.Bx[:,:,t].^2 + sim.By[:,:,t].^2)
-        Vx = sim.Ux[:,:,t]
-        heatmap(sim.xvals, sim.yvals, transpose(B))#, clims=(-1, 1), aspect_ratio=1)
-    end
-
-    return anim
 end
