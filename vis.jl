1f7a56c8fe943bad69cb9685bd8bb75bca5aef62
Jake WIllard <jacob.willard.gr@dartmouth.edu>
2022-07-14 17:33:52 -0400

    abandon Makie, write fft algorithm for streamlines

--- vis.jl
+++ vis.jl
@@ -1,9 +1,30 @@
-using CairoMakie
+using Plots
 using Interpolations
 using HDF5
 using Glob
+using FFTW
 include("./my_julia_defs.jl")
 
-dat = SimData("./output")
+dat = SimData("./bigsig")
 
-animate_magnetic_field(dat, "./test.gif", framerate=10)
+function plot_stream(x, y, Vx, Vy)
+
+    Nx = length(x)
+    Ny = length(y)
+    kx = fftfreq(Nx)*Nx
+    ky = fftfreq(Ny)*Ny
+    kxm = hcat([kx for i=1:Ny]...)
+    kym = hcat([ones(Nx)*kyp for kyp in ky]...)
+    k2 = kxm .^2 + kym .^2
+    k2[1,1] = 1.0
+
+    Vxh = fft(Vx)
+    Vyh = fft(Vy)
+    Jh = im*(kxm .* Vyh - kym .* Vxh)
+    phih = - Jh ./ k2
+    phih[1,1] = 0.0
+
+    phi = real.(ifft(phih))
+    c = contour(x, y, transpose(phi), color = :black, colorbar=false)
+    return c
+end
