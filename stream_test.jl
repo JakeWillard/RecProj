b16116bf65199f00854335596f1482428d3d763a
Jake <jacob.willard.gr@dartmouth.edu>
2022-08-17 12:43:11 -0400

    sdfgsdfg

--- stream_test.jl
+++ stream_test.jl
@@ -1,29 +1,38 @@
 # depends on: Plots, Interpolations
 
-
-
 struct FlowField
 
     f :: Function 
     Nt :: Int64 
-    dt :: Float64
+    Nl :: Int64
 
 end
 
 
-function FlowField(Ux, Uy, dx, dy, dt)
+function FlowField(Ux, Uy, dx, dy, Nl, Nt; scale=0.01)
 
-    _, _, Nt = size(Ux)
-    ux = interpolate(Ux, BSpline(Cubic()))
-    uy = interpolate(Uy, BSpline(Cubic()))
+    _, _, nt = size(Ux)
+
+    ux = interpolate(Ux, BSpline(Linear()))
+    uy = interpolate(Uy, BSpline(Linear()))
+    uxe = extrapolate(ux, Flat())
+    uye = extrapolate(uy, Flat())
     f(r, t) = begin 
         i = 1 + (r[1] + 0.5)/dx
         j = 1 + (r[2] + 0.5)/dy
-        k = t/dt 
-        Float64[ux(i,j,k), uy(i,j,k)]
+        k = 1 + ((t-1)/(Nt-1))*(nt-1)
+        scale*Float64[uxe(i,j,k), uye(i,j,k)]
     end
 
-    return FlowField(f, Nt, dt)
+    return FlowField(f, Nt, Nl)
+end
+
+
+function FlowField(dat, Nl, Nt; scale=0.01)
+
+    dx = dat.xvals[2] - dat.xvals[1]
+    dy = dat.yvals[2] - dat.yvals[1] 
+    return FlowField(dat.Ux, dat.Uy, dx, dy, Nl, Nt, scale=scale)
 end
 
 
@@ -32,68 +41,120 @@ function rk4(x, y, t, flow::FlowField)
 
     r = Vector([x, y]) 
     k1 = flow.f(r, t)
-    k2 = f(r + flow.dt*k1/2, t+flow.dt/2)
-    k3 = f(r + flow.dt*k2/2, t+flow.dt/2)
-    k4 = f(r + flow.dt*k3, t+flow.dt)
-    rnew = r + flow.dt*(k1 + 2*k2 + 2*k3 + k4)/6
+    k2 = flow.f(r + k1/2, t+0.5)
+    k3 = flow.f(r + k2/2, t+0.5)
+    k4 = flow.f(r + k3, t+1)
+    rnew = r + (k1 + 2*k2 + 2*k3 + k4)/6
 
-    return rnew[1], rnew[2], t + flow.dt
+    return rnew[1], rnew[2]
 end
 
 
 
-function streamline(x0, y0, t0, Nt, Nl, flow::FlowFIeld)
+function streamlines(x0, y0, flow::FlowField)
 
-    X = fill(NaN, (Nl, Nt))
-    Y = fill(NaN, (Nl, Nt))
-    T = fill(NaN, Nt)
+    Nt = flow.Nl * flow.Ntr
 
-    X[1:Nl+1,1] .= x0 
-    Y[1:Nl+1,1] .= y0 
-    T[1] = t0
+    X = fill(NaN, (flow.Nl, 2, Nt))
+    Y = fill(NaN, (flow.Nl, 2, Nt))
 
-    for i=2:Nt 
-        xnew, ynew, tnew = rk4(X[i-1,i-1], Y[i-1,i-1], T[i-1], flow)
+    t = 1
+    j = 1
 
-        X[i:i+Nl,i] .= xnew
-        Y[i:i+Nl,i] .= ynew
-        T[i] = tnew 
+    for _=1:flow.Ntr-1
+        
+        xn, yn = rk4(x0, y0, t, flow)
+        X[1,j,t:t+flow.Nl] .= x0 
+        X[2,j,t:t+flow.Nl] .= xn
+        Y[1,j,t:t+flow.Nl] .= y0 
+        Y[2,j,t:t+flow.Nl] .= yn
+        t += 1
+
+        for i=3:flow.Nl
+            xnew, ynew = rk4(X[i-1,j,t-1], Y[i-1,j,t-1], t-1, flow)
+            X[i,j,t:t+flow.Nl] .= xnew
+            Y[i,j,t:t+flow.Nl] .= ynew
+            t += 1
+        end 
+
+        j = (j == 1) ? 2 : 1
     end
 
     return X, Y
-end 
-
-
-function animate(Nd, Nt, Nl, flow::FlowField)
-
-    X = fill(NaN, Nl, Nd, flow.Nt)
-    Y = fill(NaN, Nl, Nd, flow.Nt)
-
 end
 
 
+function one_streamline(x0, y0, flow::FlowField; gap=0.5)
 
+    X = fill(NaN, (flow.Nl, flow.Nt))
+    Y = fill(NaN, (flow.Nl, flow.Nt))
+    t = 1
 
+    while t + 2*flow.Nl <= flow.Nt
 
+        xn, yn = rk4(x0, y0, t, flow)
+        X[1,t:t+flow.Nl-2] .= x0 
+        X[2,t:t+flow.Nl-2] .= xn
+        Y[1,t:t+flow.Nl-2] .= y0 
+        Y[2,t:t+flow.Nl-2] .= yn
+        t += 1
 
-    
+        for i=3:flow.Nl 
+            xn, yn = rk4(X[i-1,t-1], Y[i-1,t-1], t-1, flow)
+            X[i,t:t+flow.Nl-2] .= xn
+            Y[i,t:t+flow.Nl-2] .= yn
+            t += 1 
+        end 
 
+        t += Int(floor(flow.Nl*gap))
+    end
 
-    
-
-
-
-
-
-
-
-
-
-        
-
+    return X, Y
 end
-    
+
+
+function many_streamlines(Nx, Ny, flow::FlowField; gap=0.5)
+
+    X = fill(NaN, (flow.Nl, Nx*Ny, flow.Nt))
+    Y = fill(NaN, (flow.Nl, Nx*Ny, flow.Nt))
+    xs = LinRange(-0.5, 0.5, Nx)
+    ys = LinRange(-0.5, 0.5, Ny)
+    k = 0
+
+    for x in xs 
+        for y in ys 
+            k += 1 
+            Xs, Ys = one_streamline(x, y, flow, gap=gap)
+            X[:,k,:] = Xs[:,:]
+            Y[:,k,:] = Ys[:,:]
+        end 
+    end 
+
+    return X, Y 
+end
+
+            
 
 
 
 
+function more_streamlines(N, flow::FlowField)
+
+    X = fill(NaN, (flow.Nl, 2*N, flow.Nl*flow.Ntr))
+    Y = fill(NaN, (flow.Nl, 2*N, flow.Nl*flow.Ntr))
+
+    for i=1:N
+        k = 2*(i-1) + 1
+        Xi, Yi = streamlines(flow)
+        X[:,k:k+1,:] = Xi[:,:,:]
+        Y[:,k:k+1,:] = Yi[:,:,:]
+    end
+
+    return X, Y 
+end
+
+# TODO: 
+# no more randomization: each stream always starts from the same x0 y0. wozniakowski?
+# indexing seems a little weird still 
+# make the flow field a unit vector? at least fix normalization
+#  
